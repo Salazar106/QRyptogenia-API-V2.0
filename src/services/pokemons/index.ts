f6bee113bcbbe89f426a53876210334dafd1343b
Salazar106 <jdps106@hotmail.com>
2025-05-22 22:43:29 -0500

    refactor(pokemons): remove unused Pokémon service files

--- src/services/pokemons/index.ts
+++
@@ -1,9 +0,0 @@
-import {getPokemonFromAPI} from './getPokemonFromApi.service'
-import {createPokemon} from './createPokemon.service'
-import { getMyPokemonsService } from './getMyPokemons.service'
-
-export {
-    getPokemonFromAPI,
-    createPokemon,
-    getMyPokemonsService
-}
